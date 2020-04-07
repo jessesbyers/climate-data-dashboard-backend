abe7a0caf75af3b6af8fb9ac014a91d1b764f552
jessesbyers <jesse.s.byers@gmail.com>
2020-04-07 10:22:22 -0400

    added actions to charts controller

--- app/controllers/api/v1/charts_controller.rb
+++ app/controllers/api/v1/charts_controller.rb
@@ -1,2 +1,33 @@
 class API::V1::ChartsController < ApplicationController
+
+    def index
+        @charts = Chart.all
+        render json: @charts
+    end
+
+    def show
+        @chart = Chart.find(params[:id])
+        render json: @chart
+    end
+
+    def create
+        @chart = Chart.new(chart_params)
+        if @chart.save
+            render json: @chart
+        else
+            render json: {error: "Chart cannot be saved to the database. Please try again."}
+        end
+    end
+
+    def destroy
+        @chart = Chart.find(params[:id])
+        @chart.destroy
+        render json: {alert: "Chart Deleted"}
+    end
+
+    private
+
+    def chart_params
+        params.require(:chart).permit(:name, :data_url)
+    end
 end
