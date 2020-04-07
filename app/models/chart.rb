4d6516c5a40cecfb72ebdfcbf76ae49cc91feef2
jessesbyers <jesse.s.byers@gmail.com>
2020-04-07 09:25:29 -0400

    added associations and validations for Chart, Notice, and Wonder

--- app/models/chart.rb
+++ app/models/chart.rb
@@ -1,2 +1,7 @@
 class Chart < ApplicationRecord
+    has_many :notices
+    has_many :wonders
+    
+    validates :name, presence: true
+    validates :data_url, presence: true
 end
--- app/models/wonder.rb
+++ app/models/wonder.rb
@@ -1,2 +1,7 @@
 class Wonder < ApplicationRecord
+    belongs_to :chart
+
+    validates: content, presence: true
+    # validates :content, length: { minimum: 20 }
+    validates: votes, numericality: true
 end
