d6122423989f1abe37e40f6b25ef1de904b1d389
jessesbyers <jesse.s.byers@gmail.com>
2020-04-15 10:01:46 -0400

    turned on content length validation in wonder and notice models

--- app/models/wonder.rb
+++ app/models/wonder.rb
@@ -2,6 +2,6 @@ class Wonder < ApplicationRecord
     belongs_to :chart
 
     validates :content, presence: true
-    # validates :content, length: { minimum: 20 }
+    validates :content, length: { minimum: 20 }
     validates :votes, numericality: true
 end
