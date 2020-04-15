8ff5d10dd8b03001f124a283ae86e1f7117d0fdb
jessesbyers <jesse.s.byers@gmail.com>
2020-04-15 08:01:54 -0400

    added more charts to seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,15 +16,74 @@ chart3data = {
     screenshot_url: "https://i.imgur.com/nBfhBVX.png"
 }
 
+chart4data = {
+    name: "CO2 in the Atmosphere and Annual Emissions, 1750-2019",
+    data_url: "https://www.climate.gov/news-features/understanding-climate/climate-change-atmospheric-carbon-dioxide",
+    screenshot_url: "https://i.imgur.com/jrojyRM.png"
+}
+
+chart5data = {
+    name: "Sea Level since 1880",
+    data_url: "https://www.climate.gov/news-features/understanding-climate/climate-change-global-sea-level",
+    screenshot_url: "https://i.imgur.com/D2SVgzC.png"
+}
+
+chart6data = {
+    name: "Sea Level Change, 1993-2018",
+    data_url: "https://www.climate.gov/news-features/understanding-climate/climate-change-global-sea-level",
+    screenshot_url: "https://i.imgur.com/K0GFygW.png"
+}
+
+chart7data = {
+    name: "Arctic Sea Ice Extent, 1980-2020",
+    data_url: "https://www.climate.gov/news-features/understanding-climate/climate-change-minimum-arctic-sea-ice-extent",
+    screenshot_url: "https://i.imgur.com/MWqhKUT.png"
+}
+
+chart8data = {
+    name: "Arctic Sea Ice Extent by Month, 1980-2018",
+    data_url: "https://www.climate.gov/news-features/understanding-climate/climate-change-minimum-arctic-sea-ice-extent",
+    screenshot_url: "https://i.imgur.com/pXqfto1.png"
+}
+
+chart9data = {
+    name: "Arctic Sea Ice Extent Over the Last 1500 Years",
+    data_url: "https://www.climate.gov/news-features/understanding-climate/climate-change-minimum-arctic-sea-ice-extent",
+    screenshot_url: "https://i.imgur.com/SsT7dPu.png"
+}
+
+chart10data = {
+    name: "Ocean heat content in 2018 compared to the 1955-2006 average",
+    data_url: "https://www.climate.gov/news-features/understanding-climate/climate-change-ocean-heat-content",
+    screenshot_url: "https://i.imgur.com/RvhmEzY.png"
+}
+
+
+
+
 
 
 chart1 = Chart.new(chart1data)
 chart2 = Chart.new(chart2data)
 chart3 = Chart.new(chart3data)
+chart4 = Chart.new(chart4data)
+chart5 = Chart.new(chart5data)
+chart6 = Chart.new(chart6data)
+chart7 = Chart.new(chart7data)
+chart8 = Chart.new(chart8data)
+chart9 = Chart.new(chart9data)
+chart10 = Chart.new(chart10data)
 
 chart1.save
 chart2.save
 chart3.save
+chart4.save
+chart5.save
+chart6.save
+chart7.save
+chart8.save
+chart9.save
+chart10.save
 
 
 notice1 = Notice.new(content: "The temperature line zigzags up and down, but the early 1900s ziglags are lower than the early 2000s zigzags", votes: 5, chart_id: 1)
